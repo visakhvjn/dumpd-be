ff247c25e7ceca561ca68d462e483835dcb3c4ed
Visakh Vijayan <vjnvisakh@gmail.com>
2025-05-05 18:18:20 +0530

    added swagger doc

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yamljs';
 
 import {
 	getUsers,
@@ -8,6 +10,12 @@ import {
 } from '../controllers/apiController.js';
 
 const apiRouter = new express.Router();
+
+// Load Swagger specification from YAML file
+const swaggerDocument = yaml.load('./swagger.yml');
+
+// Initialize Swagger UI
+apiRouter.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 apiRouter.get('/users', getUsers);
 apiRouter.get('/users/:userId', getUser);
