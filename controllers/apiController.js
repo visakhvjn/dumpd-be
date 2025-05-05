6b97c24b467a63483310249cab7f31dc48c742a0
Visakh Vijayan <vjnvisakh@gmail.com>
2025-05-05 12:32:25 +0530

    added get user resource endpoint with validations

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,4 +1,5 @@
 import * as userService from '../services/userService.js';
+import mongoose from 'mongoose';
 
 const getBlogs = async (req, res) => {};
 const getBlog = async (req, res) => {};
@@ -6,4 +7,20 @@ const getBlog = async (req, res) => {};
 export const getUsers = async (req, res) => {
 	const users = await userService.getUsers();
 	res.json(users);
+};
+
+export const getUser = async (req, res) => {
+	const { userId } = req.params;
+
+	if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
+		res.status(400).json({ error: 'Invalid userId!' });
+	}
+
+	const user = await userService.getUser(userId);
+
+	if (!user) {
+		res.status(404).json({ error: 'User not found!' });
+	}
+
+	res.json(user);
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { getUsers } from '../controllers/apiController.js';
+import { getUsers, getUser } from '../controllers/apiController.js';
 
 const apiRouter = new express.Router();
 
 apiRouter.get('/users', getUsers);
+apiRouter.get('/users/:userId', getUser);
 
 export default apiRouter;
