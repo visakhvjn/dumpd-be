fc827f024f8e166d820b9a68be651ee4c25a8b60
Visakh Vijayan <vjnvisakh@gmail.com>
2025-05-12 12:50:42 +0530

    cron jobs are now skipped on development

--- jobs/cron.js
+++ jobs/cron.js
@@ -3,26 +3,37 @@ import cron from 'node-cron';
 import { generateUser } from '../services/user.service.js';
 import * as blogService from '../services/blog.service.js';
 
-// Every hour a new blog is added
-cron.schedule('0 * * * *', async () => {
-	console.log('⏰ Running daily blog generator...');
+const jobs = [];
 
-	try {
-		await blogService.generateBlog();
-		console.log('✅ Blog generated successfully!');
-	} catch (err) {
-		console.error('❌ Error generating blog:', err);
+export const scheduleCronJobs = () => {
+	if (process.env.NODE_ENV !== 'production') {
+		console.log(`Cronjob scheduling is skipped on ${process.env.NODE_ENV}`);
+		return;
 	}
-});
 
-// Every day at 10am a new user is added
-cron.schedule('0 10 */1 * *', async () => {
-	console.log('⏰ Running user generator...');
+	console.log(`scheduling cron jobs on ${process.env.NODE_ENV}`);
 
-	try {
-		await generateUser();
-		console.log('✅ User generated successfully!');
-	} catch (err) {
-		console.error('❌ Error generating User:', err);
-	}
-});
+	const blogJob = cron.schedule('0 * * * *', async () => {
+		console.log('⏰ Running daily blog generator...');
+
+		try {
+			await blogService.generateBlog();
+			console.log('✅ Blog generated successfully!');
+		} catch (err) {
+			console.error('❌ Error generating blog:', err);
+		}
+	});
+
+	const userJob = cron.schedule('0 10 */1 * *', async () => {
+		console.log('⏰ Running user generator...');
+
+		try {
+			await generateUser();
+			console.log('✅ User generated successfully!');
+		} catch (err) {
+			console.error('❌ Error generating User:', err);
+		}
+	});
+
+	jobs.push(blogJob, userJob);
+};
