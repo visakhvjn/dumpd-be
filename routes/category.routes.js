16a93c75e68e2e62845f05f90af2cf5fbf7dde06
Visakh Vijayan <vjnvisakh@gmail.com>
2025-05-13 20:33:25 +0530

    hiding risky routes

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -4,7 +4,5 @@ import * as categoryRoutes from '../controllers/category.controller.js';
 const router = Router();
 
 router.get('/', categoryRoutes.getCategories);
-router.post('/', categoryRoutes.createCategory);
-router.patch('/:id', categoryRoutes.updateCategory);
 
 export default router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,4 @@ import * as userController from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get('/generate', userController.generateUser);
-
 export default router;
