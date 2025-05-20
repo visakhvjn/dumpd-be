b520fb667830e7fa0eeb52b60916b9d1e99a618a
Visakh Vijayan <vjnvisakh@gmail.com>
2025-05-21 02:08:43 +0530

    fixing a db deprecated warning

--- config/db.js
+++ config/db.js
@@ -5,12 +5,7 @@ dotenv.config();
 
 export const connectDb = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
-
-		//TODO: add a ping here to check for db connection
+		await mongoose.connect(process.env.MONGO_URI);
 
 		console.log('MongoDB connected');
 	} catch (error) {
